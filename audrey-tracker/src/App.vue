b4dec9a3db08e07b258d43fcd6af24fa18b70344
Audrey  Ernesto de Lima <3970@ucdb.br>
2022-06-24 16:37:41 -0400

    iniciando o projeto

---
+++ audrey-tracker/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+
+
+    </div>
+  </main>
+
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
